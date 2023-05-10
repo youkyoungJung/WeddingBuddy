b766491ca0c95ef6b73b6c3daf943358ab73e65e
youkyoungJung <yyk834@naver.com>
2023-05-10 15:06:55 +0900

    weddingBuddy

---
+++ weddingBuddy/src/main/java/com/multicampus/kb03/weddingBuddy/WeddingBuddyApplication.java
@@ -0,0 +1,26 @@
+package com.multicampus.kb03.weddingBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeddingBuddyApplication {
+
+	public static void main(String[] args) {
+		System.setProperty("server.servlet.context-path", "/weddingBuddy");
+		SpringApplication.run(WeddingBuddyApplication.class, args);
+	}
+	
+	/* 되냐구
+	 * @Bean public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
+	 * return args -> {
+	 * 
+	 * System.out.println(">>>> 스프링부트 러너  <<<< ");
+	 * System.out.println("스프링 부트 애플리케이션 구동시 시작되는 프로그램");
+	 * 
+	 * String[] beanNames = ctx.getBeanDefinitionNames(); for (String beanName :
+	 * beanNames) { System.out.println(beanName); }
+	 * 
+	 * }; }
+	 */
+}
---
+++ weddingBuddy/src/main/webapp/WEB-INF/jsp/footer.jsp
@@ -0,0 +1,25 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+
+<!DOCTYPE html>
+<html>
+<style>
+</style>
+<head>
+<meta charset="UTF-8">
+<!-- 부트스트랩 css-->
+<link href="css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+	<hr class="mb-4">
+	<footer class="my-3 pt-3 text-muted text-center text-small">
+		<p class="mb-1">&copy; 2023 Kookmin WeddingBuddy Project</p>
+		<ul class="list-inline">
+			<li class="list-inline-item"><a href="#">Privacy</a></li>
+			<li class="list-inline-item"><a href="#">Terms</a></li>
+			<li class="list-inline-item"><a href="#">Support</a></li>
+		</ul>
+	</footer>
+</body>
+</html>
